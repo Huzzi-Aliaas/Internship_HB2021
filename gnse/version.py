8e995e7ceb8fb2ed9e4aa438ed7ccc807f605db0
omelchert <melchert@iqo.uni-hannover.de>
2021-04-27 18:20:49 +0200

    minor edit

--- gnse/version.py
+++ gnse/version.py
@@ -10,6 +10,7 @@ CHANGELOG:
 0.1.1 (Di 27 Apr 2021 17:43:30 CEST)
 ------------------------------------
 * Merged "master" with "Symmetric_split_step" brach on  -- OM
-* New class in solver.py: Symmetric_Split_Step_Solver -- HB
+* solver.py - Symmetric_Split_Step_Solver -- HB
+* solver.py - added documentation -- OM
 
 """
