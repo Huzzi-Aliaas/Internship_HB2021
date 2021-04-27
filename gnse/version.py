43092c190799bff654985b8ac93ed9cfe0e11f75
omelchert <melchert@iqo.uni-hannover.de>
2021-04-27 17:49:55 +0200

    added version file

---
+++ gnse/version.py
@@ -0,0 +1,15 @@
+"""
+Clarifies versioning for the project.
+
+"""
+__version__ = "0.1.1"
+
+"""
+CHANGELOG:
+
+0.1.1 (Di 27 Apr 2021 17:43:30 CEST)
+------------------------------------
+* Merged "master" with "Symmetric_split_step" brach on  -- OM
+* New class in solver.py: Symmetric_Split_Step_Solver -- HB
+
+"""
